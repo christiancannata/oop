a11cfb821fee5de921bb259ae97827efe2bb9b2a
christian cannata <christian1488@hotmail.it>
2022-11-09 15:02:58 +0100

    wip api

---
+++ esercizio1.php
@@ -0,0 +1,18 @@
+<?php
+/* ESERCIZIO 1
+
+Replicare il funzionamento di una rubrica, crendo una classe Rubrica che contiene al suo interno un array di contatti.
+
+Ogni contatto ha le seguenti caratteristiche:
+
+- Nome
+- Cognome
+- Telefono
+- Email
+
+La rubrica deve:
+
+- esporre un metodo per aggiungere un contatto alla rubrica
+- esporre un metodo per restituire un contatto dalla rubrica dato in input il numero di telefono
+- metodo per cancellare un contatto dalla rubrica dando in input il nome ed il cognome
+*/
